5b8d4f616a971f043c16a40c30c94e89648cdad8
akram <akram20091989@gmail.com>
2022-11-02 18:14:07 +0600

    new

--- homeWork4/main.swift
+++ homeWork4/main.swift
@@ -9,23 +9,46 @@
 //После чего эти слова сортируются по количеству символов
 //Далее отображается самое короткое слово(если несколько с таким количеством - отобразить все) и так же самое длинное
 
+print("Введите слова:")
+var answer = ""
+var words = [String]()
 
-print("Введите слова")
-
-var string = readLine()!
-var count = 0
-for char in readLine()!{
-    if readLine()! == " "{
-    }else if char == ","{
-        string += " - \(count) символов\n"
-        count = 0
-    }else{
-        count += 1
-        string += String(char)
-    }
+for _ in 1...6{
+    answer = readLine()!
+    words.append(answer)
 }
 
+print(words)
 
+var i = 0
+var longWordsArray = [String]()
+var shortWordsArray = [String]()
+var long = words[0]
+var short = words[0]
+
+while i < words.count{
+
+    if (words[i].count > long.count) {
+        longWordsArray.removeAll()
+        long = words[i]
+        longWordsArray.append(long)
+    } else if (words[i].count == long.count){
+        longWordsArray.append(words[i])
+    }
+
+    if (words[i].count < short.count) {
+        shortWordsArray.removeAll()
+        short = words[i]
+        shortWordsArray.append(short)
+    } else if (words[i].count == short.count){
+        shortWordsArray.append(words[i])
+    }
+
+    i += 1
+}
+
+print("Самые длинные слова из массива: \(longWordsArray)")
+print("Самые короткие слова из массива: \(shortWordsArray)")
 
 //№2. Составить список из 20 контактов (Имя - номер телефона)
 //Отсортировать по алфавиту (А-Я) и отобразить на какую букву сколько контактов сохранено
@@ -40,4 +63,5 @@ for char in readLine()!{
 //В игре должно быть 5 раундов по 3 вопроса
 //Так же должны быть несгораемые суммы
 //Так же в конце если человек проиграл - показать какой ответ был верным и отобразить выигрыш и сколько раундов было выиграно
+
 
